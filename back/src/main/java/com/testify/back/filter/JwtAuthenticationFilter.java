846eee588cdc2eef2156205298a0ade7cd0bd6fd
YesI4m <itissuperepic@gmail.com>
2024-11-11 21:45:20 +0900

    '주석삭제'

--- back/src/main/java/com/testify/back/filter/JwtAuthenticationFilter.java
+++ back/src/main/java/com/testify/back/filter/JwtAuthenticationFilter.java
@@ -14,14 +14,11 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 
-// ioc를 통해 di할수 있도록 컴포넌트로 등록
 @Component
-// 필수 생성자를 만들어줌 <= final로 지정된거
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter{
     private final JwtProvider jwtProvider;
 
-    // jwtauthenticationfilter의 implemented methods
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
@@ -30,10 +27,8 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter{
 
     }
 
-    // 헤더의 인증키를 찾아 bearer인증인지 확인
     private String parseBearerToken(HttpServletRequest request){
         String authorization = request.getHeader("Authorization");
-        // hasText는 null이거나 공백으로 들어가있으면 false 반환
         boolean hasAuthorization = StringUtils.hasText(authorization);
         if (!hasAuthorization) return null;
 
