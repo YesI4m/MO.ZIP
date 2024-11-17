a470bdfbb5c8e5c5ff699f3effafdac27f958fbb
YesI4m <itissuperepic@gmail.com>
2024-11-18 05:48:40 +0900

    JwtauthenticationFilter남은거 완료,WebSercurityConfig 작성

--- back/src/main/java/com/testify/back/filter/JwtAuthenticationFilter.java
+++ back/src/main/java/com/testify/back/filter/JwtAuthenticationFilter.java
@@ -2,6 +2,12 @@ package com.testify.back.filter;
 
 import java.io.IOException;
 
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -23,7 +29,35 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter{
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
 
+        try {
+                
+            String token = parseBearerToken(request);
+            if (token == null) {
+                filterChain.doFilter(request, response);
+                return;
+            }
 
+            String email = jwtProvider.validate(token);
+            
+            if (email == null){
+                filterChain.doFilter(request, response);
+                return;
+            }
+
+            AbstractAuthenticationToken authenticationToken = 
+            new UsernamePasswordAuthenticationToken(email, null, AuthorityUtils.NO_AUTHORITIES);
+
+            authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+            
+            SecurityContext SecurityContext = SecurityContextHolder.createEmptyContext();
+            SecurityContext.setAuthentication(authenticationToken);
+
+            SecurityContextHolder.setContext(SecurityContext);
+        } catch (Exception e) {
+            e.printStackTrace();
+    }
+    
+    filterChain.doFilter(request, response);
 
     }
 
